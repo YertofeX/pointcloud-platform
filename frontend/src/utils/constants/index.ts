24195e358275e028aa82f7f28e7875731d4bdf36
Dániel Szabó <szabo.daniel@pannon2010.hu>
2024-10-04 16:06:36 +0200

    setup i18next

---
+++ frontend/src/utils/constants/index.ts
@@ -0,0 +1,3 @@
+export { LOCALES } from "./locales";
+export { SUPPORTED_LANGUAGES } from "./supportedLanguages";
+export { THEMES } from "./themes";
---
+++ frontend/src/utils/constants/locales.ts
@@ -0,0 +1,4 @@
+export const LOCALES = {
+  en: "English",
+  hu: "Hungarian",
+} as const;
