0fb18ab2725daa435ad1b932511e157816345a9e
Dániel Szabó <szabo.daniel@pannon2010.hu>
2024-10-04 14:00:52 +0200

    setup router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [count, setCount] = useState(0)     
 
   return (
     <>
@@ -32,4 +31,3 @@ function App() {
   )
 }
 
-export default App
