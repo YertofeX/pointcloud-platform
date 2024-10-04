21b57039341b3c8a289775ecc5dd7cabad98f626
Dániel Szabó <szabo.daniel@pannon2010.hu>
2024-10-04 14:18:24 +0200

    setup MUI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './router.tsx'
+import { router } from './routes/router.tsx'
 
+import "./main.css" 
+
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/700.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
