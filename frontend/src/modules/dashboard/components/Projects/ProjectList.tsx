b2275e4a8dca297ea1d7869d10f2482d056e902e
Dániel Szabó <szabo.daniel@pannon2010.hu>
2024-10-09 12:19:25 +0200

    implement project list skeleton

--- frontend/src/modules/dashboard/components/Projects/ProjectList.tsx
+++ frontend/src/modules/dashboard/components/Projects/ProjectList.tsx
@@ -1,5 +1,12 @@
+import { Container, Paper } from "@mui/material";
+import { SkeletonProjectList } from "./SkeletonProjectList";
+
 type Props = {};
 
 export const ProjectList = ({}: Props) => {
-  return null;
+  return (
+    <Container component={Paper}>
+      <SkeletonProjectList />
+    </Container>
+  );
 };
