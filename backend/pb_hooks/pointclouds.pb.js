cc4409ff3914129b567877bee2caece39cddb618
Dániel Szabó <szabo.daniel@pannon2010.hu>
2024-10-24 14:54:12 +0200

    setup docker compose

--- backend/pb_hooks/pointclouds.pb.js
+++ backend/pb_hooks/pointclouds.pb.js
@@ -1,7 +1,25 @@
 /// <reference path="../pb_data/types.d.ts" />
 
 onRecordAfterCreateRequest((e) => {
-  console.log(JSON.stringify(e.httpContext, null, 4));
-  console.log(JSON.stringify(e.record, null, 4));
-  console.log(JSON.stringify(e.uploadedFiles, null, 4));
+  console.log("httpContext:", JSON.stringify(e.httpContext, null, 4));
+  console.log("record:", JSON.stringify(e.record, null, 4));
+  console.log("uploadedFiles:", JSON.stringify(e.uploadedFiles, null, 4));
+
+  const recordFilesPath = [
+    "",
+    "pb",
+    "pb_data",
+    "storage",
+    e.record.baseFilesPath(),
+  ].join("/");
+  console.log("recordFilesPath:", recordFilesPath);
+
+  const cmd = $os.cmd(
+    "/potreeconverter/PotreeConverter",
+    "-i",
+    `${recordFilesPath}/${e.record.get("raw")}`,
+    "-o"`${recordFilesPath}/${e.record.get("raw").replace(".las", ".a")}`
+  );
+  const output = toString(cmd.output());
+  console.log(output);
 }, "pointclouds");
