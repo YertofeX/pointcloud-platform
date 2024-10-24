954440f9e907f1314bf914879e7327d5312530ea
Dániel Szabó <szabodaniel35@gmail.com>
2024-10-24 12:54:37 +0200

    create shell script to start docker

--- backend/pb_hooks/pointclouds.pb.js
+++ backend/pb_hooks/pointclouds.pb.js
@@ -1,7 +1,7 @@
 /// <reference path="../pb_data/types.d.ts" />
 
 onRecordAfterCreateRequest((e) => {
-  console.log(e.httpContext);
-  console.log(e.record);
-  console.log(e.uploadedFiles);
+  console.log(JSON.stringify(e.httpContext, null, 4));
+  console.log(JSON.stringify(e.record, null, 4));
+  console.log(JSON.stringify(e.uploadedFiles, null, 4));
 }, "pointclouds");
--- backend/serve.sh
+++ backend/serve.sh
@@ -1,1 +1,2 @@
-./pocketbase serve --http="0.0.0.0:8090"
+# ./pocketbase serve --http="0.0.0.0:8090"
+docker run -it -p 8090:8090 pointcloud-platform-backend
