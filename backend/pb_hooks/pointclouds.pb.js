cb58510ee8917e824cbed9054bc513aa6d520f6c
Dániel Szabó <szabodan@pannon2010.hu>
2024-10-30 16:18:45 +0100

    modify backend dockerfile and pointclouds collection

--- backend/pb_hooks/pointclouds.pb.js
+++ backend/pb_hooks/pointclouds.pb.js
@@ -5,21 +5,35 @@ onRecordAfterCreateRequest((e) => {
   console.log("record:", JSON.stringify(e.record, null, 4));
   console.log("uploadedFiles:", JSON.stringify(e.uploadedFiles, null, 4));
 
-  const recordFilesPath = [
+  const recordFilesDir = [
     "",
     "pb",
     "pb_data",
     "storage",
     e.record.baseFilesPath(),
   ].join("/");
-  console.log("recordFilesPath:", recordFilesPath);
+
+  // TODO: try https://pocketbase.io/jsvm/functions/_filesystem.fileFromPath.html
+
+  const raw = `${recordFilesDir}/${e.record.get("raw")}`;  
+  const metadata = `${recordFilesDir}/metadata.json`;
+  const hierarchy = `${recordFilesDir}/hierarchy.bin`;
+  const octree = `${recordFilesDir}/octree.bin`;
+  const log = `${recordFilesDir}/log.txt`;
 
   const cmd = $os.cmd(
-    "/potreeconverter/PotreeConverter",
-    "-i",
-    `${recordFilesPath}/${e.record.get("raw")}`,
-    "-o"`${recordFilesPath}/${e.record.get("raw").replace(".las", ".a")}`
+    "/potreeconverter/build/PotreeConverter",
+    raw,
+    "-o",
+    recordFilesDir
   );
+  
+  console.log(JSON.stringify(cmd));
   const output = toString(cmd.output());
   console.log(output);
+  
+  e.record.set("metadata", metadata);
+  e.record.set("hierarchy", hierarchy);
+  e.record.set("octree", octree);
+  e.record.set("log", log);
 }, "pointclouds");
