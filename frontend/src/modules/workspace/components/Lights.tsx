a23a5ef46d706c2117b25892fc7004522ad9d018
Dániel Szabó <szabodan@pannon2010.hu>
2024-10-30 13:44:03 +0100

    increase ambient light intensity

--- frontend/src/modules/workspace/components/Lights.tsx
+++ frontend/src/modules/workspace/components/Lights.tsx
@@ -1,8 +1,7 @@
 export const Lights = () => {
   return (
     <>
-      <ambientLight intensity={0.1} />
-      <pointLight position={[2, 2, 2]} intensity={10} />
+      <ambientLight intensity={1} />
     </>
   );
 };
