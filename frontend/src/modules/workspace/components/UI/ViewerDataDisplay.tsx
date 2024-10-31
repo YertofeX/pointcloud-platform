4b9447305513211bc95b13509eb5fd03cf08ac59
Dániel Szabó <szabo.daniel@pannon2010.hu>
2024-10-31 17:05:54 +0100

    implement project files page layout

--- frontend/src/modules/workspace/components/UI/ViewerDataDisplay.tsx
+++ frontend/src/modules/workspace/components/UI/ViewerDataDisplay.tsx
@@ -1,36 +1,15 @@
-import { Avatar, Paper, Stack, styled, Typography } from "@mui/material";
-import { useWorkspaceContext } from "../WorkspaceContext/WorkspaceContext";
-import { pocketBase } from "@lib/pocketbase";
-import { Construction as ConstructionIcon } from "@mui/icons-material";
+import { styled } from "@mui/material";
+import { WorkspaceDataDisplay } from "./WorkspaceDataDisplay";
 
 export const ViewerDataDisplay = () => {
-  const { project } = useWorkspaceContext();
-
   return (
-    <DataDisplayPaper variant="outlined">
-      <Stack direction="row" gap={2} alignItems="center" pr={{ xs: 0, md: 2 }}>
-        <Avatar
-          sx={{ width: 40, height: 40 }}
-          variant="rounded"
-          src={
-            project.thumbnail !== ""
-              ? pocketBase.getFileUrl(project, project.thumbnail, {
-                  thumb: "0x64",
-                })
-              : undefined
-          }
-        >
-          <ConstructionIcon />
-        </Avatar>
-        <Typography maxWidth={200} noWrap display={{ xs: "none", md: "block" }}>
-          {project.name}
-        </Typography>
-      </Stack>
+    <DataDisplayPaper>
+      <WorkspaceDataDisplay />
     </DataDisplayPaper>
   );
 };
 
-const DataDisplayPaper = styled(Paper)({
+const DataDisplayPaper = styled("div")({
   position: "absolute",
   top: 10,
   left: 60,
