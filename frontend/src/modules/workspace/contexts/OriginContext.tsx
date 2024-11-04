dad9718d8fb15becb5961ecfa0ea2d75288a9d4f
Dániel Szabó <szabodaniel35@gmail.com>
2024-11-04 22:03:41 +0100

    implement origin context

---
+++ frontend/src/modules/workspace/contexts/OriginContext.tsx
@@ -0,0 +1,59 @@
+import { useLocalStorage } from "@mantine/hooks";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { Vector3 } from "three";
+import { useWorkspaceContext } from "../components/WorkspaceContext/WorkspaceContext";
+
+type OriginContextType = {
+  transform: Vector3;
+  setTransform: (newTransform: Vector3) => void;
+};
+
+const OriginContext = createContext<OriginContextType>({
+  transform: new Vector3(),
+  setTransform: () => {},
+});
+
+export const useOriginContext = () => useContext(OriginContext);
+
+export const OriginProvider = ({ children }: PropsWithChildren) => {
+  const {
+    project: { id: projectID },
+  } = useWorkspaceContext();
+  const [lsTransform, setLsTransform] = useLocalStorage<{
+    x: number;
+    y: number;
+    z: number;
+  }>({
+    key: `viewer-transform-${projectID}`,
+    defaultValue: { x: 0, y: 0, z: 0 },
+    getInitialValueInEffect: false,
+  });
+
+  const [transform, setTransform] = useState<Vector3>(
+    new Vector3(lsTransform.x, lsTransform.y, lsTransform.z)
+  );
+
+  const handleSetTransform = (newTransform: Vector3) => {
+    const { x, y, z } = newTransform;
+    setLsTransform({ x, y, z });
+    setTransform(newTransform);
+  };
+
+  useEffect(() => {
+    console.log({ transform });
+  }, [transform]);
+
+  return (
+    <OriginContext.Provider
+      value={{ transform, setTransform: handleSetTransform }}
+    >
+      {children}
+    </OriginContext.Provider>
+  );
+};
